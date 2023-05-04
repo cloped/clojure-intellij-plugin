9dd2fd468ffa34b6b832eae5f642525bb84292e6
GitHub Action <action@github.com>
2023-05-04 21:52:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/cloped/clojureintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/cloped/clojureintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cloped.clojureintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/test/kotlin/com/github/cloped/clojureintellijplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/cloped/clojureintellijplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cloped.clojureintellijplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.cloped.clojureintellijplugin.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
